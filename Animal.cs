4e77a6e575a5eb86d81be691044de968dabe5a8f
Ahmed Ansari <ahmedansari153@gmail.com>
2016-04-07 13:27:46 -0500

    Ayoooo

--- Animal.cs
+++ Animal.cs
@@ -1,19 +1,16 @@
 class Animal {
     // Simple properties
-    public double speed { get; set; }
-    public string species { get; set; }
     public int legs { get; set; }
+    public int Eyes { get; set; }
     // Public method that can be redefined by derived classes
 }
 
-class Species : Animal {
-    public string SpeciesName { get; set; }
+class Genus : Animal {
+    public string GenusName { get; set; }
 }
 
-
-
 // Derived class
-class Lizard : Species {
+class Lizard : Genus {
     // Adding additional properties to what is inherited from Animal
     public string Noise { get; set; }
     public string scaleColor { get; set; }
@@ -21,43 +18,84 @@ class Lizard : Species {
     public int Stripes { get; set; }
 }
 
-class Dog : Species {
+class Salamander : Lizard {
+    public bool Slimy { get; set; }
+    public int Speed { get; set; }
+}
+
+class Iguana : Lizard {
+    public string Color { get; set; }
+    public string EyeShape { get; set; }
+}
+
+class BeardedDragon : Lizard {
+    public int Size { get; set ;}
+    public bool Poison { get; set; }
+}
+
+class Monitor : Lizard {
+
+}
+
+class KomodoDragon : Lizard {
+
+}
+
+class Dog : Genus {
     public string Noise { get; set; }
     public int Spots { get; set; }
     public bool Big { get; set; }
     public string Breed { get; set; }
 }
 
-class Parrot : Species {
+class Rottweiler : Dog {
+    private int Aggressiveness = 0;
+}
+
+class Labrador : Dog {
+    public string Color { get; set; }
+}
+
+class Poodle : Dog {
+    public string Size { get; set; }
+}
+
+class Wolf: Dog {
+    public bool Howls { get; set; }
+}
+
+class Bear : Dog {
+    public string Location { get; set; }
+}
+
+class Bird : Genus {
     public string Noise { get; set; }
     public string Type { get; set; }
     public bool CanFly { get; set; }
     public int FlightDistance { get; set; }
 }
 
-Dog buddy = new Dog();
+class SunConure : Bird {
+    public bool Screams { get; set; }
+    public string Foods { get; set; }
+    public SunConure (string Fruit) {
+        Foods = Fruit;
+    }
+}
 
-buddy.Noise = "loud";
-buddy.Spots = 20;
-buddy.Big = true;
-buddy.Breed = "Labrador Retriever";
+class Pigeon : Bird {
+    public string Location { get; set; }
+}
 
-Console.Write("I have a Dog named Buddy who makes a noise that sounds {0}. He has {1} spots. His breed is {2}.", buddy.Noise, buddy.Spots, buddy.Breed);
+class Macaw : Bird {
+    public string Size { get; set;}
+}
 
-Parrot peaches = new Parrot();
+class AfricanGrey : Bird {
+    public string Color { get; set; }
+}
 
-peaches.Noise = "loud";
-peaches.Type = "Sun Conure";
-peaches.CanFly = true;
-peaches.FlightDistance = 25;
-
-Console.Write("I have a Parrot named Peaches who makes a noise that sounds {0}. It is {1} that he can fly. He is a {2}.", peaches.Noise, peaches.CanFly, peaches.Type);
-
-Lizard sam = new Lizard();
-
-sam.Noise = "quiet";
-sam.scaleColor = "Green";
-sam.camouflage = false;
-sam.Stripes = 4;
-
-Console.Write("I have a Lizzard named Sam who makes a noise that sounds {0}. It is {1} . It is {2} that he can camouflage.", sam.Noise, sam.scaleColor, sam.camouflage);
+class Penguin : Bird {
+    public bool Gender { get; set; }
+    private string _food = "Fish"; 
+}
